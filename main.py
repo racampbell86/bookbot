c08cade050d48b5c646f7007d882007e7cd10465
racampbell86 <racampbell86@gmail.com>
2024-11-23 21:10:19 -0600

    Added count of each letter in book

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
+    letters_dic = {}
+    #count the number of letters
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    count = len(file_contents.split())
+    print(f"{count} words in the book")
+#count the number of times each letter occurs
+    
+    lowered_string = file_contents.lower()
+
+    # Iterate over each character in the string
+    for letter in lowered_string:
+    # Check if the character is in the dictionary
+        if letter in letters_dic:
+        # Increment the count if it exists
+            letters_dic[letter] += 1
+        else:
+        # Initialize the count if it's not in the dictionary
+            letters_dic[letter] = 1
+    print(letters_dic)    
 main()
